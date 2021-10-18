b6bfce0085532e1927a1e6b2cb357e25c09126c6
jdvpl <juanda554242@gmail.com>
2021-10-18 14:22:42 -0500

    useefect

--- 1basicos/src/components/Hook.js
+++ 1basicos/src/components/Hook.js
@@ -1,15 +1,21 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function Hook(props) {
 
     const [stateCar, setCar] = useState(false);
     const [stateIncrease, setstateIncrease] = useState(0);
+    const [stateContar, setstateContar] = useState(0);
 
 
 const EncenderApagar=()=>{
     // setCar(!stateCar);
     setCar(prevValue=>!prevValue);
+    setstateContar(stateContar+1);
 }
+
+useEffect(() => {
+    console.log('Total '+stateContar)
+}, [stateContar])
 
 const IncreaseNumeric=()=>{
     setstateIncrease(prevValue=> prevValue+1);
@@ -18,7 +24,7 @@ const IncreaseNumeric=()=>{
     return(
         <div>
             <h2>Carro esta {stateCar ? "Encendido" : "Apagado"} </h2>
-
+            <h4>Click: {stateContar}</h4>
             <button 
                 onClick={EncenderApagar}
             >{stateCar ? "Apagar" : "Encender"}</button>
