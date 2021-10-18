57e466c478f36d2a0a0e20d18f97e81c03c811c9
jdvpl <juanda554242@gmail.com>
2021-10-18 15:59:09 -0500

    integrando semantic ui diseño

--- proyecto/ecommerce-client/README.md
+++ proyecto/ecommerce-client/README.md
@@ -5,3 +5,14 @@
 npm install -g sass
 npm install --save-dev sass
 `
+
+
+### instalar semantic UI 
+
+---
+https://react.semantic-ui.com/usage
+---
+
+`
+npm install semantic-ui-react semantic-ui-css
+`
--- proyecto/ecommerce-client/pages/index.js
+++ proyecto/ecommerce-client/pages/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { Button, Icon, Label } from 'semantic-ui-react'
 
 export default function Home() {
   return (
     <div className="home">
       <h1>Next js</h1>
+
+      <Button as='div' labelPosition='right'>
+      <Button color='red'>
+        <Icon name='heart' />
+        Like
+      </Button>
+      <Label as='a' basic color='red' pointing='left'>
+        2,048
+      </Label>
+    </Button>
+
     </div>
   )
 }
