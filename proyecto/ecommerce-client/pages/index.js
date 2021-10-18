454ad754ba8db9156468a03fea00c940bcb1a511
jdvpl <juanda554242@gmail.com>
2021-10-18 16:19:51 -0500

    creando layoutbasico y el sass de ese sas en el global sass

--- proyecto/ecommerce-client/pages/index.js
+++ proyecto/ecommerce-client/pages/index.js
@@ -1,21 +1,13 @@
 import React from 'react'
-import { Button, Icon, Label } from 'semantic-ui-react'
+import BasicLayout from '../layouts/LayoutBasic'
 
 export default function Home() {
   return (
     <div className="home">
+
+      <BasicLayout>
       <h1>Next js</h1>
-
-      <Button as='div' labelPosition='right'>
-      <Button color='red'>
-        <Icon name='heart' />
-        Like
-      </Button>
-      <Label as='a' basic color='red' pointing='left'>
-        2,048
-      </Label>
-    </Button>
-
+      </BasicLayout>
     </div>
   )
 }
